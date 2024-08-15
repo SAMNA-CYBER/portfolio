df42b5c57032de787b0280e7cb41dd3a0328f98b
samna <samnasaif666@gmail.com>
2024-08-15 08:40:14 +0530

    portfoilio commit1

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function sendMail(event) {
+    event.preventDefault(); // Prevent form submission and page refresh
+
+    var params = {
+        name: document.getElementById("uname").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_y1tf12z", "template_8o4fg4c", params)
+    .then(function(res) {
+        alert("Success! Email sent. Status: " + res.status);
+    })
+    .catch(function(error) {
+        console.error("EmailJS Error:", error); // Log the error to the console
+        alert("Failed to send email. Please try again.");
+    });
+
+}
